6c2b50b9d9ffaa4146decc51e8988cc519c070e1
li1622 <1622719703@qq.com>
2021-08-16 16:02:41 +0800

    111

---
+++ src/com/yr/ABC.java
@@ -0,0 +1,18 @@
+package com.yr;
+
+public class ABC {
+
+    public static void main(String[] args) {
+        String str ="123";
+        int a =Integer.parseInt(str);
+        System.out.println(a);
+
+        int b =123456;
+        String c =String.valueOf(b);
+        System.out.println(c);
+
+    }
+
+
+
+}
